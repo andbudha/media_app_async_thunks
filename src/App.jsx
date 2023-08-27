9bc997a57c8ab52027c682f109d26e2fac9e9e9b
andbudha <andreibartov@gmail.com>
2023-08-27 15:17:35 +0200

    Button/Panel & UsersList comp-s added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { UsersList } from './components/UsersList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div>
-           <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <UsersList/>
       </div>
   )
 }
---
+++ src/components/UsersList.jsx
@@ -0,0 +1,7 @@
+
+
+export const UsersList =()=>{
+  return(
+    <div className="container mx-auto">UsersList</div>
+  );
+}
