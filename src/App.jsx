220d49efa11482d89cc3a3fa1f5562ed974154c4
andbudha <andreibartov@gmail.com>
2023-08-28 11:31:49 +0200

    user-list fetching data implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import { UsersList } from './components/UsersList'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
 
-
-export const UsersList =()=>{
-  return(
-    <div className="container mx-auto">UsersList</div>
-  );
-}
+export const UsersList = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.users.data);
+  console.log(users);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  return <div className="container mx-auto">UsersList</div>;
+};
