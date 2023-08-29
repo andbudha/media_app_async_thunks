957d4e8abc0392f2c644b6abcb2bad807cff6846
andbudha <andreibartov@gmail.com>
2023-08-29 15:45:17 +0200

    ExpandablePanel component created

--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -2,6 +2,7 @@ import { GoTrash } from 'react-icons/go';
 import Button from './Button';
 import { useThunk } from '../hooks/use-thunk';
 import { deleteUser } from '../store';
+import { ExpandabalePanel } from './ExpandablePanel';
 
 export const UsersListItem = ({ user }) => {
   const [doDeleteUser, isLoading, error] = useThunk(deleteUser);
@@ -9,17 +10,16 @@ export const UsersListItem = ({ user }) => {
   const handleClick = () => {
     doDeleteUser(user);
   };
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <GoTrash />
-          </Button>
-          {error && <div>Error deleting user!</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+
+  const header = (
+    <>
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <GoTrash />
+      </Button>
+      {error && <div>Error deleting user!</div>}
+      {user.name}
+    </>
   );
+
+  return <ExpandabalePanel header={header}>Content!!!</ExpandabalePanel>;
 };
