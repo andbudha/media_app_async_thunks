5a47122ee9338813035ede53c57cf7814cbf6d01
andbudha <andreibartov@gmail.com>
2023-08-29 14:47:40 +0200

    deleting user func-ty updated

--- src/store/slices/usersSlice.jsx
+++ src/store/slices/usersSlice.jsx
@@ -38,8 +38,7 @@ const usersSlice = createSlice({
     });
     builder.addCase(deleteUser.fulfilled, (state, action) => {
       state.isLoading = false;
-      //fix me
-      console.log(action);
+      state.data = state.data.filter((user) => user.id !== action.payload.id);
     });
     builder.addCase(deleteUser.rejected, (state, action) => {
       state.isLoading = false;
--- src/store/thunks/deleteUser.jsx
+++ src/store/thunks/deleteUser.jsx
@@ -2,7 +2,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const deleteUser = createAsyncThunk('users/delete', async (user) => {
-  const response = axios.delete(`http://localhost:3005/users/${user.id}`);
-
-  return response.data;
+  axios.delete(`http://localhost:3005/users/${user.id}`);
+  return user;
 });
