0a1e41e5d6b14223b9a851ef90b65c25aec29659
andbudha <andreibartov@gmail.com>
2023-09-03 17:15:06 +0200

    rtk query albums fetching functionality added

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,3 +1,8 @@
+import { useFetchAlbumsQuery } from '../store';
+
 export const AlbumsList = ({ user }) => {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  console.log(data, error, isLoading);
   return <div>Albums for {user.name}</div>;
 };
--- src/store/apis/albumsApi.jsx
+++ src/store/apis/albumsApi.jsx
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
 const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({
