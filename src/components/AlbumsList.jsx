8b2c7fc94e04b501055d5c0ffed5228befc5639b
andbudha <andreibartov@gmail.com>
2023-09-04 10:43:53 +0200

    albums fetching functionality added

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,8 +1,30 @@
 import { useFetchAlbumsQuery } from '../store';
+import { Skeleton } from './Skeleton';
+import { ExpandabalePanel } from './ExpandablePanel';
 
 export const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums!</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandabalePanel key={album.id} header={header}>
+          List of photos in the album!
+        </ExpandabalePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 };
