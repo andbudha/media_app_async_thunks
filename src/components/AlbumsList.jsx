5b31865fc601930e25156e0fe7cb4f87c5c8fcd4
andbudha <andreibartov@gmail.com>
2023-09-04 14:55:04 +0200

    AlbumsListItem comp&delete button added/styled

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,7 +1,7 @@
 import { useAddAlbumMutation, useFetchAlbumsQuery } from '../store';
 import { Skeleton } from './Skeleton';
-import { ExpandabalePanel } from './ExpandablePanel';
 import Button from './Button';
+import { AlbumsListItem } from './AlbumsListItem';
 
 export const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
@@ -12,17 +12,12 @@ export const AlbumsList = ({ user }) => {
   };
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className={'h-10 w-full'} times={3} />;
   } else if (error) {
     content = <div>Error loading albums!</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandabalePanel key={album.id} header={header}>
-          List of photos in the album!
-        </ExpandabalePanel>
-      );
+      return <AlbumsListItem key={album.id} album={album}></AlbumsListItem>;
     });
   }
 
