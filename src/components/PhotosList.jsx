4d815aefe6d332f988247c37eeb6c4f893205184
andbudha <andreibartov@gmail.com>
2023-09-06 10:30:44 +0200

    addPhoto endpoint updated

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,3 +1,23 @@
+import { useAddPhotoMutation, useFetchPhotosQuery } from '../store';
+import Button from './Button';
+
 export const PhotosList = ({ album }) => {
-  return <div>PhotosList</div>;
+  const { data, isFetching, error } = useFetchPhotosQuery(album);
+
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const addPhotoHandler = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={addPhotoHandler}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 };
